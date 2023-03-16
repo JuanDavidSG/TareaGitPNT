8521e2a4669803c7c3d172ae834790ae0169f939
Andreafm <andreaformon@unisabana.edu.co>
2023-03-15 20:32:22 -0500

    Agregar punto y coma en el objetivo

--- src/Clima.java
+++ src/Clima.java
@@ -2,6 +2,6 @@ public class Clima {
     void anunciar ()
     {
         System.out.println("Este es un anuncio del clima");
-        System.out.println("La finalidad de anunciar el clima es tener un registro diario de este ya que es muy variable")
+        System.out.println("La finalidad de anunciar el clima es tener un registro diario de este ya que es muy variable");
     }
 }
