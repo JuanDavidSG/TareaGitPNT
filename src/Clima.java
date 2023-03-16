1189665e1cb9ff8962da740dd41ad6229568d282
Andreafm <andreaformon@unisabana.edu.co>
2023-03-15 20:35:43 -0500

    Cambiar mensaje de bienvenida

--- src/Clima.java
+++ src/Clima.java
@@ -1,7 +1,7 @@
 public class Clima {
     void anunciar ()
     {
-        System.out.println("Este es un anuncio del clima");
+        System.out.println("Bienvenidos al programa del clima");
         System.out.println("La finalidad de anunciar el clima es tener un registro diario de este ya que es muy variable");
     }
 }
