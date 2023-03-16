080decf7bf55401f6d08b66c51033930e8b8ae68
Andreafm <andreaformon@unisabana.edu.co>
2023-03-15 20:56:05 -0500

    Dar título de periódico

--- src/Clima.java
+++ src/Clima.java
@@ -1,7 +1,7 @@
 public class Clima {
     void anunciar ()
     {
-        System.out.println("Bienvenidos al programa del clima");
+        System.out.println("Bienvenidos al periódico informativo del clima");
         System.out.println("La finalidad de anunciar el clima es tener un registro diario de este ya que es muy variable");
     }
 }
